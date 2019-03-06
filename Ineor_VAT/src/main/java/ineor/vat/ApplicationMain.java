9f405c1b8fa02115c394afca9ca5eb853e27347f
Oleg <olegzinin@gmail.com>
2019-03-06 18:37:40 +0500

    removed exceptions

--- Ineor_VAT/src/main/java/ineor/vat/ApplicationMain.java
+++ Ineor_VAT/src/main/java/ineor/vat/ApplicationMain.java
@@ -1,9 +1,5 @@
 package ineor.vat;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-import org.apache.tomcat.util.json.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
@@ -15,7 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Oleg
  */
 public class ApplicationMain {
-	    public static void main(String[] args) throws MalformedURLException, IOException, ParseException {
+	    public static void main(String[] args) {
 	    	SpringApplication.run(ApplicationMain.class, args);
 	    }
 	}
